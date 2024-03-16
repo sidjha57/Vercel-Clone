5d9549dcb242d5c065a549bec2eeda537378700e
Siddharth Jha <sidjha57@gmail.com>
2024-03-16 23:41:39 +0530

    feat: initial commit

--- upload-service/src/utils/generateUniqueID.ts
+++
@@ -1,8 +0,0 @@
-export function generateUniqueID (): string {
-    let ans = ''
-    const subset = '1234567890qwertyuiopasdfghjklzxcvbnm'
-    for (let i = 0; i < (process.env.ID_MAX_LEN as unknown as number ?? 5); i++) {
-      ans += subset[Math.floor(Math.random() * subset.length)]
-    }
-    return ans
-  }
