8d43ba9d1defad04b12a236c05eb88c7420291d9
Siddharth Jha <sidjha57@gmail.com>
2024-03-19 20:02:15 +0530

    feat: initial commit

---
+++ reverse-proxy-service/src/index.ts
@@ -0,0 +1,20 @@
+import { logger } from './utils/logger';
+
+const object = {
+  myname: 'siddharth',
+  your: {
+    name: 'siddharth',
+    age: 34
+  }
+};
+
+async function hello (): Promise<void> {
+  for (let i = 0; i < 5; i++) {
+    setTimeout(() => {
+      logger.debug('Hello World', object);
+    }, 1000 * i);
+  }
+}
+
+await hello();
+export { hello };
