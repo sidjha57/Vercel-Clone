cd2ce0dac4bb3d3d1fc611443a14dc0bb292433e
Siddharth Jha <sidjha57@gmail.com>
2024-03-20 00:54:56 +0530

    chore: docker image size reduced

--- build-service/main.sh
+++ build-service/main.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/ash
 
 export GIT_REPOSITORY__URL="$GIT_REPOSITORY__URL"
 
-git clone "$GIT_REPOSITORY__URL" /home/app/output
+git clone "$GIT_REPOSITORY__URL" /app/output
 
 exec node script.js
