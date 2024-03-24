b6da84e80899528ae79794bb8fe1010e92fa86f5
Siddharth Jha <sidjha57@gmail.com>
2024-03-24 11:35:53 +0530

    feat: reverse proxy completed

--- reverse-proxy-service/src/index.ts
+++ reverse-proxy-service/src/index.ts
@@ -1,20 +1,46 @@
+import express from 'express';
 import { logger } from '@/utils/logger';
+import httpProxy from 'http-proxy';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const object = {
-  myname: 'siddharth',
-  your: {
-    name: 'siddharth',
-    age: 34
+const app = express();
+const PORT = process.env.PORT || 8000;
+let BASE_PATH = process.env.BASE_PATH || '';
+
+BASE_PATH = BASE_PATH.endsWith('/') ? BASE_PATH.substring(0, BASE_PATH.length - 1) : BASE_PATH;
+
+const proxy = httpProxy.createProxy();
+
+app.use((req, res) => {
+  try {
+    const hostname = req.hostname;
+    logger.debug('hostname', hostname);
+    const subdomain = hostname.split('.')[0];
+
+    // We can add custom domain too by adding a mapping between custom domain and ID using DB
+
+    // logger.debug('Requested asset ', subdomain);
+
+    // logger.debug('Base Path', BASE_PATH);
+
+    const resolvesTo = `${BASE_PATH}/${subdomain}`;
+
+    // logger.debug('Resolves to ', resolvesTo);
+    return proxy.web(req, res, { target: resolvesTo, changeOrigin: true });
+  } catch (err: any) {
+    logger.error(err);
+    return res.status(500).json({ message: err.message });
   }
-};
+});
 
-async function hello (): Promise<void> {
-  for (let i = 0; i < 5; i++) {
-    setTimeout(() => {
-      logger.debug('Hello World', object);
-    }, 1000 * i);
-  }
-}
+proxy.on('proxyReq', (proxyReq, req, res) => {
+  const url = req.url;
+  if (url === '/')
+    proxyReq.path += 'index.html';
+    return proxyReq;
+})
 
-await hello();
-export { hello };
+app.listen(PORT, () => {
+  logger.info(`Reverse Proxy Server started on ${PORT}`);
+});
